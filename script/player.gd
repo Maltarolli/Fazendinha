75a41ecaf87e002b47750c77621aeef3d352c2f5
Pedro Roberto <maltarollipedro@gmail.com>
2024-11-04 20:32:21 -0300

    Initial commit

---
+++ script/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var velocidade_player : float = 125.00
+var direcao_player : Vector2 = Vector2(0, 0)
+
+func _ready() -> void:
+	pass 
+
+
+func _process(delta: float) -> void:
+	movimentacao()
+
+
+func movimentacao() -> void: 
+	if Input.is_action_pressed("mv_right"):
+		direcao_player.x = 1
+	elif Input.is_action_pressed("mv_left"):
+		direcao_player.x = -1
+	elif Input.is_action_pressed("mv_up"):
+		direcao_player.y = -1
+	elif Input.is_action_pressed("mv_down"):
+		direcao_player.y = 1
+	else:
+		direcao_player.x = 0
+		direcao_player.y = 0
+		
+	velocity = direcao_player.normalized() * velocidade_player
+	move_and_slide()
