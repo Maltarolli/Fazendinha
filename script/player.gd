b5fcecb8eda59a4b5bac1018ec0672147bc5901e
Pedro Roberto <maltarollipedro@gmail.com>
2024-11-04 22:08:32 -0300

    Tomate
    Fazendo o tomate crescer ao tocar

--- script/player.gd
+++ script/player.gd
@@ -14,15 +14,22 @@ func _process(delta: float) -> void:
 func movimentacao() -> void: 
 	if Input.is_action_pressed("mv_right"):
 		direcao_player.x = 1
+		$AnimatedSprite2D.play("direita")
 	elif Input.is_action_pressed("mv_left"):
 		direcao_player.x = -1
-	elif Input.is_action_pressed("mv_up"):
+		$AnimatedSprite2D.play("esquerda")
+	else:
+		direcao_player.x = 0
+	if Input.is_action_pressed("mv_up"):
 		direcao_player.y = -1
 	elif Input.is_action_pressed("mv_down"):
 		direcao_player.y = 1
 	else:
-		direcao_player.x = 0
 		direcao_player.y = 0
+		
+	if direcao_player.x == 0 and direcao_player.y == 0:
+		$AnimatedSprite2D.play("idle")
+		
 		
 	velocity = direcao_player.normalized() * velocidade_player
 	move_and_slide()
