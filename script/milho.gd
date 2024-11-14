f8fdf6fefe43de4c7470fbb10853ecee4792420a
Pedro Roberto <maltarollipedro@gmail.com>
2024-11-14 10:56:44 -0300

    organização

--- script/milho.gd
+++ script/milho.gd
@@ -5,14 +5,12 @@ var mouse_milho : bool = false
 var encostou : bool = false
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimatedSprite2D.frame = 0
 	crescer()
 	Dados.sementes -= 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	colher()
 
