715fb274cde63257975aa49576aa96b4c1c40233
Pedro Roberto <maltarollipedro@gmail.com>
2024-11-22 23:56:15 -0300

    melhoria no derrubar da arvore

--- script/arvore.gd
+++ script/arvore.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 var encostou : bool = false
-var pode_cortar : bool = true
+var cortou : bool = false
+var vida : int = 100
+var dropou : bool = false
 
 var node_graveto = preload("res://scene/graveto.tscn")
 var node_folha = preload("res://scene/folha_seca.tscn")
 
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("normal") 
+	pass
 
 
 func _process(delta: float) -> void:
@@ -24,12 +26,38 @@ func _on_body_exited(body: Node2D) -> void:
 	
 	
 func cortar() -> void:
-	if encostou and Input.is_action_just_released("click_left") and pode_cortar:
+	
+	if encostou and Input.is_action_just_released("click_left") and !cortou and vida > 0:
+		#Tira 20 de vida da arvore
+		vida -= 20
+		#Inicia a animação dela cortando
 		$AnimatedSprite2D.play("cortando")
-		await get_tree().create_timer(5.0).timeout
-		$AnimatedSprite2D.play("cortada")
-		pode_cortar = false
+		#Timer de 0.5 segundos
+		await get_tree().create_timer(0.5).timeout
+		#Para a animação
+		$AnimatedSprite2D.stop()
+		#deixa a animação no 0, que parece ela parada 
+		$AnimatedSprite2D.frame = 0
+	
+		#Verifica se a vida é zero
+		if vida == 0:
+			$AnimatedSprite2D.play("cortada")
+			cortou = true
+			#Volta a vida dela a 100 para esse if nao ficar em loop 
+			vida = 100
+		if cortou:
+			drop()
+			await get_tree().create_timer(5.0).timeout
+			$AnimatedSprite2D.play("normal")
+			cortou = false
+			dropou = false
+			
 
+
+#Função para dropar as folhas e galhos
+func drop() -> void:
+	if !dropou:
+		print("oi")
 		#dropa o graveto apos cortar
 		var instance_graveto = node_graveto.instantiate()
 		instance_graveto.position = $drop_graveto.position
@@ -39,13 +67,5 @@ func cortar() -> void:
 		var instance_folha = node_folha.instantiate()
 		instance_folha.position = $drop_folha.position
 		add_child(instance_folha)
-		
-		#chama oma mama
-		crescer()
-		
-		
-func crescer() -> void:
-	if pode_cortar == false:
-		await get_tree().create_timer(5.0).timeout
-		pode_cortar = true
-		$AnimatedSprite2D.play("normal")
+		dropou = true
+	
